3ebd886b60a71b19d94cb0ecd585ed07e6017e04
Qiao Xiaomeng <ayame9joe@gmail.com>
2016-01-31 02:28:04 +0800

    Add new rules , multi hits and level up

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -17,12 +17,26 @@ public class GameManager : MonoBehaviour {
 	private float timeTemp = 0f;
 	private bool countTime;
 
+	private float holdTime;
+	private float holdTimeTemp;
+	private float holdTimeInterval;
+
+	private float stopTime;
+	private float stopTimeTemp;
+	private float stopTimeInterval;
+
 	private int swordNumber;
+
+	private int hitTimes;
+
+	private int level;
+
+
 
 	// Use this for initialization
 	void Start () {
 
-		InvokeRepeating ("ThoughtsInitiation", 0, Random.Range (0, 0.5f));
+		InvokeRepeating ("InitiationThoughts", 0, Random.Range (0, 0.5f));
 
 	}
 
@@ -31,27 +45,73 @@ public class GameManager : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 
+
 			Meditation ();
 
+			holdTimeTemp = holdTime;
+			holdTime = 0;
+
+			stopTimeTemp = stopTime;
+			stopTime = 0;
+
+			Debug.Log (stopTimeInterval);
+
+		}
+
+		if (Input.GetKey (KeyCode.Space)) {
+
+			holdTime += 1f;
+
+			holdTimeInterval = (holdTime - holdTimeTemp);
+
+		} else {
+
+			stopTime += 1f;
+
+			stopTimeInterval = stopTime - stopTimeTemp;
+
+		
+
+		}
+
+		if (timeInterval < 1) {
+			hitTimes++;
+		} else {
+			hitTimes = 0;
+		}
+
+		if (hitTimes > 5) {
+			level++;
+			hitTimes = 0;
 		}
 
 	}
 
-	void ThoughtsInitiation () {
+	/// <summary>
+	/// Initiations the thoughts.
+	/// </summary>
+	void InitiationThoughts () {
 
 		GameObject go = Instantiate (thought, new Vector2 (Random.Range (-Defines.radius, Defines.radius), 
 			Random.Range (-Defines.radius, Defines.radius)), Quaternion.identity) as GameObject;
 
 	}
 
+
+	/// <summary>
+	/// Calculate and init sword.
+	/// </summary>
 	void Meditation () {
 	
-		SwordNumberCalculation ();
-		SwordInitiation ();
+		CalculationSwordNumber ();
+		InitiationSword ();
 
 	}
 
-	void SwordNumberCalculation () {
+	/// <summary>
+	/// Calculations the sword number.
+	/// </summary>
+	void CalculationSwordNumber () {
 
 		if (!countTime) {
 			timeInterval = Time.time - timeTemp;
@@ -66,11 +126,16 @@ public class GameManager : MonoBehaviour {
 			
 	}
 
-	void SwordInitiation () {
+	/// <summary>
+	/// Initiations the sword.
+	/// </summary>
+	void InitiationSword () {
 
 		for (int i = 0; i <= swordNumber; i++) {
 			Instantiate (sword, new Vector2 (0, 0), Quaternion.identity);
 		}
 
 	}
+
+
 }
