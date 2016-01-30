782d8f8d22bf2d0e05de7583c8cb8d21697fb89b
Qiao Xiaomeng <ayame9joe@gmail.com>
2016-01-30 14:54:02 +0800

    Build project

---
+++ Assets/Script/Defines.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Defines {
+
+	public static float radius = 10f;
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject thought;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating ("InitThoughts", 0, Random.Range (0, 5));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void InitThoughts () {
+
+		GameObject go = Instantiate (thought, new Vector2 (Random.Range (-Defines.radius, Defines.radius), 
+			Random.Range (-Defines.radius, Defines.radius)), Quaternion.identity) as GameObject;
+
+	}
+}
